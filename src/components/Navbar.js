296c271e7d4d22226f1083af6539735533d90a4c
RatishNyaichyai <ratish@fusemachines.com>
2022-12-02 10:16:02 +0545

    some changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Navbar = () => {
     return (
         <nav className='navbar'>
             <section className='logo'>NOTEBOOK</section>
             <ul className='nav-links'>
-                <li className='nav-item'><a href='/register'>Register</a></li>
+                <li className='nav-item'><Link to='/register'>Register</Link></li>
                 <li className='nav-ltem'><a href='/login'>Login</a></li>
             </ul>
         </nav>
